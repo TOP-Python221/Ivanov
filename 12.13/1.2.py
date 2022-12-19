eb17adaa5f16c3aa7d6c6acfe5caf0c32ed795f3
sergei11919ze <108266652+sergei11919ze@users.noreply.github.com>
2022-12-19 18:39:12 +0300

    Add files via upload

---
+++ 12.13/1.2.py
@@ -0,0 +1,80 @@
+
+
+
+class HTMLTag:                              # создание элементов
+    default_indent_spaces: int = 2
+
+    def __init__(self, name: str, value: str = '', **kwargs):
+        self.name = name
+        self.atr = ''
+        if kwargs:
+            for i in kwargs:
+                self.i = kwargs[i]
+                self.atr += str(f' {i}="{self.i}" ')
+        self.value = value
+        self.__nested: list[HTMLTag] = []
+
+    @property
+    def nested(self):
+        return iter(self.__nested)
+
+    @nested.setter
+    def nested(self, value: 'HTMLTag'):
+        self.__nested += [value]
+
+
+    def __str(self, indent_level: int):
+        
+        margin = ' ' * indent_level * self.default_indent_spaces
+        eol = ''
+        result = f'{margin}<{self.name}{self.atr}>{self.value}'
+        if self.__nested:
+            for tag in self.nested:
+                result += '\n' + tag.__str(indent_level + 1)
+            eol = f'\n{margin}'
+        result += f'{eol}</{self.name}>'
+        return result
+     
+
+    def __str__(self):
+        return self.__str(0)
+
+    @staticmethod
+    def create(name: str, value: str = ''):
+        return HTMLBuilder(name, value)
+
+class HTMLBuilder:
+    def __init__(self, root, value: str = ''):
+        if isinstance(root, HTMLTag):
+            self.root = root
+        elif isinstance(root, str):
+            self.root = HTMLTag(root, value)
+        else:
+            raise TypeError('Не те параметры')
+
+    def nested(self, name: str, value: str = '', **kwargs):
+        tag = HTMLTag(name, value, **kwargs)
+        self.root.nested = tag
+        return HTMLBuilder(tag)
+
+    def sibling(self, name: str, value: str = '', **kwargs):
+        tag = HTMLTag(name, value, **kwargs)
+        self.root.nested = tag
+        return self
+
+
+    def bulid(self):
+        return self.root
+
+
+root = HTMLTag.create('div')
+root.sibling('li', 'File', scr='class_name', clas='hhhhhhhhhhh').sibling('li', 'Edit').sibling('li', 'view').nested('div', 'jjjjjjjj').nested('div', 'hhhhhhhhh')
+div = root.bulid()
+print(div)
+
+
+
+
+
+
+    
