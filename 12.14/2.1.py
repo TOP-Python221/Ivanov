f21337ea91a6a8a7f21b0e5c22840e75e20787d5
sergei11919ze <sergei11919ze@gmail.com>
2023-01-01 17:05:55 +0300

    загрузка

---
+++ 12.14/2.1.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+
+
+# Passenger & Cargo Carriers
+class Carrier(ABC):
+    @abstractmethod
+    def carry_military(self, items):
+        pass
+
+    @abstractmethod
+    def carry_commercial(self, items):
+        pass
+
+
+# Military & Commercial Planes
+class Plane(ABC):
+    @abstractmethod
+    def display_description(self):
+        pass
+
+    @abstractmethod
+    def add_objects(self, new_objects):
+        pass
+    
+class PassengerCarrier(Carrier):
+    def carry_military(self, items):
+        print(f'Пассажиры {items} ', end=' ')
+        
+    def carry_commercial(self, items):
+        print(f'Пассажиры {items}', end=' ')
+        
+class CargoCarrier(Carrier):
+    def carry_military(self, items):
+        print(f'Грузы {items}')
+        
+    def carry_commercial(self, items):
+        print(f'Грузы {items}')
+        
+class MilitaryPlane(Plane):
+    def display_description(self):
+        print('доставил военный самолет')
+
+    def add_objects(self, new_objects):
+        pass
+    
+class CommercialPlane(Plane):
+    def display_description(self):
+        print('доставил гражданский самолет')
+
+    def add_objects(self, new_objects):
+        pass
+
+p = PassengerCarrier()
+c = CargoCarrier()
+mil = MilitaryPlane()
+com = CommercialPlane()
+
+p.carry_military('военные')
+mil.display_description()
+p.carry_commercial('гражданские')
+com.display_description()
+
+
+
+
+
