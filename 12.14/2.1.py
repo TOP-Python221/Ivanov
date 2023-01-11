250bb818295de064c6ace302f51e144491a70116
Gennadiy <renardes@gmail.com>
2023-01-11 12:54:04 +0500

    12.14: правки удобства чтения кода

--- 12.14/2.1.py
+++ 12.14/2.1.py
@@ -21,34 +21,39 @@ class Plane(ABC):
     @abstractmethod
     def add_objects(self, new_objects):
         pass
-    
+
+
 class PassengerCarrier(Carrier):
     def carry_military(self, items):
         print(f'Пассажиры {items} ', end=' ')
         
     def carry_commercial(self, items):
         print(f'Пассажиры {items}', end=' ')
-        
+
+
 class CargoCarrier(Carrier):
     def carry_military(self, items):
         print(f'Грузы {items}')
         
     def carry_commercial(self, items):
         print(f'Грузы {items}')
-        
+
+
 class MilitaryPlane(Plane):
     def display_description(self):
         print('доставил военный самолет')
 
     def add_objects(self, new_objects):
         pass
-    
+
+
 class CommercialPlane(Plane):
     def display_description(self):
         print('доставил гражданский самолет')
 
     def add_objects(self, new_objects):
         pass
+
 
 p = PassengerCarrier()
 c = CargoCarrier()
@@ -60,11 +65,11 @@ mil.display_description()
 p.carry_commercial('гражданские')
 com.display_description()
 
-'''Пассажиры военные  доставил военный самолет
-Пассажиры гражданские доставил гражданский самолет'''
+
+# В мустат загрузаю архив потому что .txt не загружается
 
 
-'''В мустат загрузаю архив потому что .txt не загружается'''
+# stdout:
 
-
-
+# Пассажиры военные  доставил военный самолет
+# Пассажиры гражданские доставил гражданский самолет
